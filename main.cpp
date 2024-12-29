1325635a38b1654857675acc29f35670395919b7
Parth Bhatt <paarthbhatt37@gmail.com>
2024-12-30 04:27:03 +0530

    modified 2 files

--- main.cpp
+++ main.cpp
@@ -1,8 +1,17 @@
 
+// #include<iostream>
+
+// int main() {
+//     std::cout << "Hey, Parth!" << std::endl;
+//     std::cout << "I'm Learning C++" << std::endl;
+//     return 0;
+// }
+
 #include<iostream>
+using namespace std;
 
 int main() {
-    std::cout << "Hey, Parth!" << std::endl;
-    std::cout << "I'm Learning C++" << std::endl;
+    cout << "Hey, Parth!" << endl;
+    cout << "I'm Learning C++" << endl;
     return 0;
 }
