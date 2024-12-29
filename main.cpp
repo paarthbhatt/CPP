4ad913e1afd8663e652848181538997eed2b486e
Parth Bhatt <paarthbhatt37@gmail.com>
2024-12-30 04:33:22 +0530

    modifies main.cpp

--- main.cpp
+++ main.cpp
@@ -5,13 +5,22 @@
 //     std::cout << "Hey, Parth!" << std::endl;
 //     std::cout << "I'm Learning C++" << std::endl;
 //     return 0;
-// }
+// }]
 
+// This is a C++ program that prints a message to the console.
+// The '#include<iostream>' directive includes the iostream standard file to perform input/output operations.
 #include<iostream>
+
+// The 'using namespace std;' directive allows the use of standard library elements without prefixing them with 'std::'.
 using namespace std;
 
+// The 'int main()' function is the entry point of the program where execution begins.
 int main() {
-    cout << "Hey, Parth!" << endl;
-    cout << "I'm Learning C++" << endl;
-    return 0;
+    // The 'cout' object is used to insert data into the output stream.
+    // The '<<' operator is the insertion operator that inserts data into the output stream.
+    // The 'endl' manipulator is used to insert a new line and flush the buffer.
+    cout << "Hey, Parth!" << endl;  // Print a greeting message.
+    cout << "I'm Learning C++" << endl;  // Print a message indicating the learning process.
+    return 0;  // Return an integer value indicating the program's execution status.
 }
+
