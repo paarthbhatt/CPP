8315039d304d89aadf8afe700eb28293bb4b071e
Parth Bhatt <paarthbhatt37@gmail.com>
2025-01-01 22:44:48 +0530

    commit changes to main.cpp

--- main.cpp
+++ main.cpp
@@ -7,28 +7,19 @@
 //     return 0;
 // }]
 
-// // This is a C++ program that prints a message to the console.
-// // The '#include<iostream>' directive includes the iostream standard file to perform input/output operations.
-//         #include<iostream>
+// This is a C++ program that prints a message to the console.
+// The '#include<iostream>' directive includes the iostream standard file to perform input/output operations.
+        #include<iostream>
 
-// // The 'using namespace std;' directive allows the use of standard library elements without prefixing them with 'std::'.
-//         using namespace std;
+// The 'using namespace std;' directive allows the use of standard library elements without prefixing them with 'std::'.
+        using namespace std;
 
-// // The 'int main()' function is the entry point of the program where execution begins.
-//         int main() {
-//     // The 'cout' object is used to insert data into the output stream.
-//     // The '<<' operator is the insertion operator that inserts data into the output stream.
-//     // The 'endl' manipulator is used to insert a new line and flush the buffer.
-//             cout << "Hey, Parth!" << endl;  // Print a greeting message.
-//             cout << "I'm Learning C++" << endl;  // Print a message indicating the learning process.
-//             return 0;  // Return an integer value indicating the program's execution status.
-//         }
-
-#include<iostream>
-using namespace std;
-int main() {
-    int x;
-    cin >> x;
-    cout << "Value of x: " << x;
-    return 0;
-}
+// The 'int main()' function is the entry point of the program where execution begins.
+        int main() {
+    // The 'cout' object is used to insert data into the output stream.
+    // The '<<' operator is the insertion operator that inserts data into the output stream.
+    // The 'endl' manipulator is used to insert a new line and flush the buffer.
+            cout << "Hey, Parth!" << endl;  // Print a greeting message.
+            cout << "I'm Learning C++" << endl;  // Print a message indicating the learning process.
+            return 0;  // Return an integer value indicating the program's execution status.
+        }
