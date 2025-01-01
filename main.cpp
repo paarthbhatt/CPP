ae72e3774567f2fa2d87facc942d1d069dce5994
Parth Bhatt <paarthbhatt37@gmail.com>
2025-01-02 04:22:50 +0530

    changes made to the code to run the program

--- main.cpp
+++ main.cpp
@@ -1,25 +1,48 @@
 
+// // #include<iostream>
+
+// // int main() {
+// //     std::cout << "Hey, Parth!" << std::endl;
+// //     std::cout << "I'm Learning C++" << std::endl;
+// //     return 0;
+// // }]
+
+// // This is a C++ program that prints a message to the console.
+// // The '#include<iostream>' directive includes the iostream standard file to perform input/output operations.
+//         #include<iostream>
+
+// // The 'using namespace std;' directive allows the use of standard library elements without prefixing them with 'std::'.
+//         using namespace std;
+
+// // The 'int main()' function is the entry point of the program where execution begins.
+//         int main() {
+//     // The 'cout' object is used to insert data into the output stream.
+//     // The '<<' operator is the insertion operator that inserts data into the output stream.
+//     // The 'endl' manipulator is used to insert a new line and flush the buffer.
+//             cout << "Hey, Parth!" << endl;  // Print a greeting message.
+//             cout << "I'm Learning C++" << endl;  // Print a message indicating the learning process.
+//             return 0;  // Return an integer value indicating the program's execution status.
+//         }
+
+
 // #include<iostream>
+// using namespace std;
 
 // int main() {
-//     std::cout << "Hey, Parth!" << std::endl;
-//     std::cout << "I'm Learning C++" << std::endl;
+//     int x, y; // Declare variables on the same line
+//     cout << "Enter values for x and y: ";
+//     cin >> x >> y;
+//     cout << "Value of x: " << x << " and y: " << y; // Use double quotes consistently
 //     return 0;
-// }]
+// }
 
-// This is a C++ program that prints a message to the console.
-// The '#include<iostream>' directive includes the iostream standard file to perform input/output operations.
-        #include<iostream>
+#include<bits/stdc++.h>
+using namespace std;
 
-// The 'using namespace std;' directive allows the use of standard library elements without prefixing them with 'std::'.
-        using namespace std;
-
-// The 'int main()' function is the entry point of the program where execution begins.
-        int main() {
-    // The 'cout' object is used to insert data into the output stream.
-    // The '<<' operator is the insertion operator that inserts data into the output stream.
-    // The 'endl' manipulator is used to insert a new line and flush the buffer.
-            cout << "Hey, Parth!" << endl;  // Print a greeting message.
-            cout << "I'm Learning C++" << endl;  // Print a message indicating the learning process.
-            return 0;  // Return an integer value indicating the program's execution status.
-        }
+int main() {
+    int x, y; 
+    cout << "Enter values for x and y: ";
+    cin >> x >> y;
+    cout << "Value of x: " << x << " and y: " << y;
+    return 0;
+}
