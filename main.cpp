3ead2330d09add7c8d2265918021148555e258b1
Parth Bhatt <paarthbhatt37@gmail.com>
2025-01-02 12:16:02 +0530

    EXECUTING C++ PROGRAM

--- main.cpp
+++ main.cpp
@@ -90,3 +90,12 @@
 //     return 0;
 // }
 
+#include<bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int x, y;
+    cin >> x >> y;
+    cout << "Value of x: " << x << " and y: " << y;
+    return 0;
+}
