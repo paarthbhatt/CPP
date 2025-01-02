5710c1019da9d656977f8542a7988b8cae116c71
Parth Bhatt <paarthbhatt37@gmail.com>
2025-01-02 12:02:29 +0530

    modified main.cpp

--- main.cpp
+++ main.cpp
@@ -47,3 +47,46 @@
 //     return 0;
 // }
 
+// If-Else Statements
+
+// #include<bits/stdc++.h>
+// using namespace std;
+
+// int main() {
+// int age=10;
+
+
+// if (age >= 18) {
+//     cout << "You are an adult." << endl;
+// } else {
+//     cout << "You are not an adult." << endl;
+// }
+
+// }
+
+// #include<iostream>
+// using namespace std;
+
+// int main() {
+//     int marks = 84;
+
+
+//     if (marks < 25) {
+//         cout << "Grade: F" << endl;
+//     } else if (marks >= 25 && marks <= 44) {
+//         cout << "Grade: E" << endl;
+//     } else if (marks >= 45 && marks <= 49) {
+//         cout << "Grade: D" << endl;
+//     } else if (marks >= 50 && marks <= 59) {
+//         cout << "Grade: C" << endl;
+//     } else if (marks >= 60 && marks <= 69) {
+//         cout << "Grade: B" << endl;
+//     } else if (marks >= 70) {
+//         cout << "Grade: A" << endl;
+//     } else {
+//         cout << "Invalid marks entered." << endl;
+//     }
+
+//     return 0;
+// }
+
