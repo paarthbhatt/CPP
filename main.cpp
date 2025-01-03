12fdabb7adcb2d26dcdb87d52f7ff33e2f488596
Parth Bhatt <paarthbhatt37@gmail.com>
2025-01-04 01:46:22 +0530

    modify odd/even cpp program

--- main.cpp
+++ main.cpp
@@ -217,3 +217,18 @@
 //     }
 //     return 0;
 // }
+
+// Conditional Statements in For Loop:
+#include<iostream>
+using namespace std;
+
+int main() {
+    for (int i = 1; i <= 10; i++) {
+        if (i % 2 == 0) {
+            cout << i << " is an even number." << endl;
+        } else {
+            cout << i << " is an odd number." << endl;
+        }
+    }
+    return 0;
+}
