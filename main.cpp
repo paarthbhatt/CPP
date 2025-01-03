0c9d1991390c47917998544e8f443394dc729f4d
Parth Bhatt <paarthbhatt37@gmail.com>
2025-01-03 21:10:29 +0530

    added char case in switch statements

--- main.cpp
+++ main.cpp
@@ -118,22 +118,42 @@
 // }
     
 
+// #include <iostream>
+// using namespace std;
+
+// int main() {
+//     const int x = 10;
+//     const int y = 10;
+
+//     switch (x + y) {
+//         case 15:
+//             cout << "Result is 15." << endl;
+//             break;
+//         case 20:
+//             cout << "Result is 20." << endl;
+//             break;
+//         default:
+//             cout << "No match found." << endl;
+//     }
+
+//     return 0;
+// }
+
 #include <iostream>
 using namespace std;
 
 int main() {
-    const int x = 10;
-    const int y = 10;
+    char grade = 'B';
 
-    switch (x + y) {
-        case 15:
-            cout << "Result is 15." << endl;
+    switch (grade) {
+        case 'A':
+            cout << "Excellent!" << endl;
             break;
-        case 20:
-            cout << "Result is 20." << endl;
+        case 'B':
+            cout << "Good!" << endl;
             break;
         default:
-            cout << "No match found." << endl;
+            cout << "Not specified." << endl;
     }
 
     return 0;
