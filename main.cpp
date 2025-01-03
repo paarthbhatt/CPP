f355cd84715907c114c3273968157ea75b244489
Parth Bhatt <paarthbhatt37@gmail.com>
2025-01-03 21:54:19 +0530

    stage changes

--- main.cpp
+++ main.cpp
@@ -47,6 +47,10 @@
 //     return 0;
 // }
 
+
+//______________________________________________________________________________________________________________________________________________________
+
+
 // If-Else Statements
 
 // #include<bits/stdc++.h>
@@ -116,7 +120,10 @@
     
 //     return 0;
 // }
-    
+
+//______________________________________________________________________________________________________________________________________________________
+
+// Switch Statements
 
 // #include <iostream>
 // using namespace std;
@@ -159,27 +166,29 @@
 //     return 0;
 // }
 
-#include <iostream>
-using namespace std;
+// #include <iostream>
+// using namespace std;
 
-int main() {
-    int x = 2;
-    int y = 3;
+// int main() {
+//     int x = 2;
+//     int y = 3;
 
-    switch (x) {
-        case 1:
-            cout << "x is 1." << endl;
-            switch (y) {
-                case 1:
-                    cout << "y is 1." << endl;
-                    break;
-                default:
-                    cout << "y is not 1." << endl;
-            }
-            break;
-        default:
-            cout << "x is not 1." << endl;
-    }
+//     switch (x) {
+//         case 1:
+//             cout << "x is 1." << endl;
+//             switch (y) {
+//                 case 1:
+//                     cout << "y is 1." << endl;
+//                     break;
+//                 default:
+//                     cout << "y is not 1." << endl;
+//             }
+//             break;
+//         default:
+//             cout << "x is not 1." << endl;
+//     }
 
-    return 0;
-}
+//     return 0;
+// }
+
+// __________________________________________________________________________________________________________________________________________________________________________________
