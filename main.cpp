c446aa3c6d9db8c7f815a5c367322af26e7a89ad
Parth Bhatt <paarthbhatt37@gmail.com>
2025-01-04 01:28:14 +0530

    modified main.cpp

--- main.cpp
+++ main.cpp
@@ -205,3 +205,15 @@
 //     return 0;
 // }
 
+#include <iostream>
+using namespace std;
+
+int main() {
+    for (int i = 0; i < 3; i++) {
+        for (int j = 0; j < 3; j++) {
+            cout << "i = " << i << ", j = " << j << endl;
+            // Nested loop code
+        }
+    }
+    return 0;
+}
