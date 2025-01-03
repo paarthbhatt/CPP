ee7f19b21c0999f3c8ed3d60735811daf21b289a
Parth Bhatt <paarthbhatt37@gmail.com>
2025-01-03 18:52:36 +0530

    add switchcase queries

--- main.cpp
+++ main.cpp
@@ -118,3 +118,23 @@
 // }
     
 
+#include <iostream>
+using namespace std;
+
+int main() {
+    const int x = 10;
+    const int y = 10;
+
+    switch (x + y) {
+        case 15:
+            cout << "Result is 15." << endl;
+            break;
+        case 20:
+            cout << "Result is 20." << endl;
+            break;
+        default:
+            cout << "No match found." << endl;
+    }
+
+    return 0;
+}
