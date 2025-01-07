00130a07c71aba498b5a5e716a12f9e790b8fa0f
Parth Bhatt <paarthbhatt37@gmail.com>
2025-01-07 21:19:09 +0530

    made changes in 3 files

--- src/main.cpp
+++ src/main.cpp
@@ -402,3 +402,41 @@
 //     return 0;
 // }
 
+#include<bits/stdc++.h>
+using namespace std;
+
+// void doSomething(int &num) {
+//     cout << num << endl;
+//     num += 5;
+//     cout << num << endl;
+//     num += 5;
+//     cout << num << endl;
+// }
+// int main() {
+//     int num = 10;
+//     doSomething(num);
+//     cout << num << endl;
+//     return 0;
+// }
+
+// void doSomething(int arr[], int n) {
+//     if (n > 0) {
+//         arr[0] += 100;
+//         cout << "Value inside function: " << arr[0] << endl;
+//     }
+// }
+
+// int main() {
+//     int n;
+//     cout << "Enter the number of elements: ";
+//     cin >> n;
+    
+//     if (n <= 0) return 1;
+    
+//     int arr[n];
+//     for (int i = 0; i < n; i++) cin >> arr[i];
+    
+//     doSomething(arr, n);
+//     cout << "Value inside int main: " << arr[0] << endl;
+//     return 0;
+// }
