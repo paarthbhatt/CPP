707d3020562ba4f4ea3508a9d99a06d4bd2e35d2
Parth Bhatt <paarthbhatt37@gmail.com>
2025-02-06 19:14:50 +0530

    added datatypes.cpp

---
+++ SDE_DSA_Roadmap/datatypes.cpp
@@ -0,0 +1,45 @@
+// 1. Integer
+// Syntax
+// int variable_name;
+// Example
+// int number = 100;
+
+// 2. Character
+// Syntax
+// char variable_name;
+// Example
+// char letter = 'A';
+
+// 3. Boolean
+// Syntax
+// bool variable_name;
+// Example
+// bool isValid = true;
+
+// 4. Floating Point
+// Syntax
+// float variable_name;
+// Example
+// float pi = 3.14f;
+
+// 5. Double Floating Point
+// Syntax
+// double variable_name;
+// Example
+// double e = 2.718281828459045;
+
+// 6. Void or Valueless
+// Syntax
+// void function_name() {
+//     // function body
+// }
+// Example
+// void displayMessage() {
+//     cout << "Hello, World!";
+// }
+
+// 7. Wide Character
+// Syntax
+// wchar_t variable_name;
+// Example
+// wchar_t letter = L'A';
