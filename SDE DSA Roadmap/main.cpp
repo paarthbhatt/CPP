606828f1aa81b86609f079e2641cc5e7e3f7937d
Parth Bhatt <paarthbhatt37@gmail.com>
2025-02-06 16:03:10 +0530

    added 5 files

---
+++ SDE DSA Roadmap/main.cpp
@@ -0,0 +1,100 @@
+// #include <iostream>
+
+// int main()
+// {
+// 	std::cout << "How many times have you watched the DAREDEVIL? " << std::endl;
+// 	int n = 0;
+// 	std::cin >> n;
+// 	std::cout << "So, you have watched the DAREDEVIL " << n << " times" << std::endl;
+
+// 	return 0;
+// }
+
+// #include <iostream>
+// using namespace std;
+
+// int main()
+// {
+// 	// Printing the sentence
+// 	cout << "Enter a single letter: " << endl;	
+
+// 	char a;
+
+// 	// User giving input as character
+// 	cin >> a;	
+
+// 	cout << "The letter is " << a << endl;	
+// 	return 0;
+// }
+
+// #include <iostream>
+// using namespace std;
+
+// int main()
+// {
+// 	// Printing the sentence
+// 	cout << "Enter your name: " << endl;	
+
+// 	string a;
+
+// 	// User giving input as string
+// 	cin >> a;	
+
+// 	// Printing the input we took above
+// 	cout << "The name is " << a << endl;	
+
+// 	return 0;
+// }
+
+// #include <iostream>
+
+//  using namespace std;
+//  int main()
+//  {
+//  	// Making float variable
+//  	float n;	
+
+//  	// Printing the sentence
+//  	cout << "Enter your rooms temperature (in Kelvin) : " << endl;	
+
+//  	// User giving input as float value
+//  	cin >> n;	
+
+//  	// Printing the input we took above
+//  	cout << "The temperature is " << n << " kelvin " << endl;	
+
+//  	return 0;
+//  }
+
+// #include <iostream>
+// using namespace std;
+
+// int main()
+// {
+// 	// Making integer  variable already has its value
+// 	int n = 8;	
+
+// 	// Printing the input we took above
+// 	cout << "The age  is " << n << endl;	
+
+// 	return 0;
+// }
+
+// #include <iostream>
+// using namespace std;
+
+// int main()
+// {
+// 	int n;
+
+// 	// Printing the sentence
+// 	cout << "Enter your roll no : " << endl;	
+
+// 	// User giving input as integer
+// 	cin >> n;	
+
+// 	// Printing the input we took above
+// 	cout << "The roll no is " << n << endl;	
+
+// 	return 0;
+// }
