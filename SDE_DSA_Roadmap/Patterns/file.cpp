6a3a26d013f009d8e7c5275be921d5acf602d7d1
Parth Bhatt <paarthbhatt37@gmail.com>
2025-05-28 01:54:49 +0530

    made changes

--- SDE_DSA_Roadmap/Patterns/file.cpp
+++ SDE_DSA_Roadmap/Patterns/file.cpp
@@ -49,10 +49,21 @@ void pattern4(int N)
     }
 }
 
+void pattern5(int N){
+    for (int i = 0; i < N; i++)
+    {
+        for (int j = N; j > i; j--)
+        {
+            cout <<"* ";
+        }
+        cout << endl;
+    }
+}
+
 int main()
 {   
     int N = 5;
-    pattern4(N);
+    pattern5(N);
     return 0;
 }
 
